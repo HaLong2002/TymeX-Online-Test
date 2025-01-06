9206fac3958df4725696ae45aef46538289db2fd
HaLong <halong122002@gmail.com>
2025-01-06 17:02:57 +0700

    modify ui

--- Currency Converted/app/src/main/java/com/example/currency_converted/MainActivity.kt
+++ Currency Converted/app/src/main/java/com/example/currency_converted/MainActivity.kt
@@ -5,9 +5,11 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.EditText
-import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,14 +18,19 @@ import com.example.currency_converted.data.remote.APIService
 import com.example.currency_converted.data.remote.ApiUtils
 import com.example.currency_converted.model.CodesResponse
 import com.example.currency_converted.model.ExchangeRateResponse
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var input_amount: EditText
     private lateinit var input_converted: EditText
-    private lateinit var from_currency: Spinner
-    private lateinit var to_currency: Spinner
+    private lateinit var base_currency: MaterialAutoCompleteTextView
+    private lateinit var target_currency: MaterialAutoCompleteTextView
+    private lateinit var exchange_rate: TextView
+    private lateinit var swap_button: FloatingActionButton
     private lateinit var mAPIService: APIService
+    private var rates: Map<String, Double> = emptyMap()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,39 +42,73 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        from_currency = findViewById(R.id.from_currency);
-        to_currency = findViewById(R.id.to_currency);
+        base_currency = findViewById(R.id.dropdown_menu1);
+        target_currency = findViewById(R.id.dropdown_menu2);
         input_amount = findViewById(R.id.input_amount);
         input_converted = findViewById(R.id.input_converted);
+        exchange_rate = findViewById(R.id.exchange_rate);
+        val rootLayout = findViewById<ViewGroup>(R.id.main)
+        swap_button = findViewById(R.id.swap_button)
+
+//        rootLayout.setOnTouchListener { v, event ->
+//            if (currentFocus != null) {
+//                val rect = Rect()
+//                currentFocus!!.getGlobalVisibleRect(rect)
+//                if (!rect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+//                    currentFocus!!.clearFocus() // Remove focus from AutoCompleteTextView
+//                }
+//            }
+//            false
+//        }
 
         mAPIService = ApiUtils.getApiService();
 
-        getCodes()
+        getCurrencyCodes()
+
+        // Show dropdown when clicked
+        base_currency.setOnClickListener {base_currency.showDropDown()}
+        base_currency.setOnItemClickListener { parent, view, position, id ->
+            // Get the selected item
+            val selectedCurrency = parent.getItemAtPosition(position).toString().split("-")[0].trim()
+            base_currency.setText(selectedCurrency)
+            val target = target_currency.text.toString()
+            getExchangeRates(selectedCurrency, target)
+        }
+
+        target_currency.setOnClickListener {
+            target_currency.showDropDown()
+        }
+        target_currency.setOnItemClickListener { parent, view, position, id ->
+            // Get the selected item
+            val selectedCurrency = parent.getItemAtPosition(position).toString().split("-")[0].trim()
+            target_currency.setText(selectedCurrency)
+            val base = base_currency.text.toString()
+            getExchangeRates(base, selectedCurrency)
+        }
 
         input_amount.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 if(p0.isNullOrEmpty())
                     input_converted.setText("")
                 val userInput = p0.toString().trim().toDoubleOrNull()
                 if (userInput != null) {
-                    val from = from_currency.selectedItem.toString()
-                    val to = to_currency.selectedItem.toString()
-                    val base = from.split(",")[0].trim().replace("[", "").replace("]", "")
-                    val symbol = to.split(",")[0].trim().replace("[", "").replace("]", "")
-                    Log.d("from_currenty", from)
-                    Log.d("to_currency", to)
-                    getExchangeRates(base, symbol, userInput)
+                    val base = base_currency.text.toString()
+                    val target = target_currency.text.toString()
+                    Log.d("base currency", base)
+                    Log.d("target currency", target)
+                    convertExchangeRate(userInput, target)
                 }
             }
-
             override fun afterTextChanged(p0: Editable?) {}
+        })
 
+        swap_button.setOnClickListener(View.OnClickListener {
+            swap_Currency()
         })
     }
 
-    private fun getCodes() {
+    private fun getCurrencyCodes() {
         mAPIService.getCodes()
             .enqueue(object : retrofit2.Callback<CodesResponse> {
                 override fun onResponse(
@@ -77,29 +118,39 @@ class MainActivity : AppCompatActivity() {
                     if (response.isSuccessful) {
                         val body = response.body()
                         if (body != null) {
-                            // Handle the response
-                            val codes: List<List<String>> = body.supportedCodes
-                            populateSpinner(from_currency, codes)
-                            populateSpinner(to_currency, codes)
-                            println("Codes: $body")
+                            val currencyCodes: List<String> = body.supportedCodes.map { "${it[0]} - ${it[1]}" }
+                            showCurrencyCodes(currencyCodes)
+                            Log.i("Codes:", "$body")
+                            Log.i("Drop Down:", "$currencyCodes")
                         } else {
                             println("Response body is null")
                         }
-                        Log.i(ContentValues.TAG, "Get data successfully" + response.code())
                     } else {
-                        // Handle the error
                         Log.e("Error", "${response.code()}")
                     }
                 }
 
                 override fun onFailure(call: retrofit2.Call<CodesResponse>, t: Throwable) {
-                    // Handle failure
                     Log.i(ContentValues.TAG, "Failed to fetch data: ${t.message}")
                 }
             })
     }
 
-    private fun getExchangeRates(base: String, symbol: String, amount: Double) {
+    private fun showCurrencyCodes(currencyCodes: List<String>) {
+        val adapter1 = ArrayAdapter(this@MainActivity, R.layout.list_item, currencyCodes)
+        base_currency.setAdapter(adapter1)
+        val adapter2 = ArrayAdapter(this@MainActivity, R.layout.list_item, currencyCodes)
+        target_currency.setAdapter(adapter2)
+
+        if (adapter1.count > 0 && adapter2.count > 0) {
+            base_currency.setText(adapter1.getItem(0).toString().split("-")[0].trim(), false)
+            target_currency.setText(adapter2.getItem(0).toString().split("-")[0].trim(), false)
+            getExchangeRates(adapter1.getItem(0).toString().split("-")[0].trim(),
+                adapter2.getItem(0).toString().split("-")[0].trim())
+        }
+    }
+
+    private fun getExchangeRates(base: String, target: String) {
         mAPIService.getExchangeRates(base)
             .enqueue(object : retrofit2.Callback<ExchangeRateResponse> {
                 override fun onResponse(
@@ -109,42 +160,30 @@ class MainActivity : AppCompatActivity() {
                     if (response.isSuccessful) {
                         val body = response.body()
                         if (body != null) {
-                            // Handle the response
-                            val rates: Map<String, Double> = body.conversion_rates
-                            println("Exchange rates: $body")
-                            val result: Double = amount * rates[symbol]!!
-                            input_converted.setText(result.toString())
+                            Log.i("Exchange rates: ", "$body")
+                            rates = body.conversion_rates
+                            exchange_rate.setText("1 ${base} = ${rates[target]} ${target}")
                         } else {
                             println("Response body is null")
                         }
-                        Log.i(ContentValues.TAG, "Get data successfully" + response.code())
                     } else {
-                        // Handle the error
                         Log.e("Error", "${response.code()}")
                         Log.d("URL", call.request().url().toString())
                     }
                 }
 
                 override fun onFailure(call: retrofit2.Call<ExchangeRateResponse>, t: Throwable) {
-                    // Handle failure
                     Log.i(ContentValues.TAG, "Failed to fetch data: ${t.message}")
                 }
             })
     }
 
-    private fun populateSpinner(spinner: Spinner, data: List<List<String>>) {
-        // Create an ArrayAdapter
-        val adapter = ArrayAdapter(
-            spinner.context,
-            android.R.layout.simple_spinner_item,
-            data
-        )
-
-        // Set the dropdown layout style
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
-        // Attach the adapter to the Spinner
-        spinner.adapter = adapter
+    private fun convertExchangeRate(amount: Double, target: String) {
+        val result: Double = amount * rates[target]!!
+        input_converted.setText(result.toString())
     }
 
+    private fun swap_Currency() {
+
+    }
 }
--- Currency Converted/app/src/main/java/com/example/currency_converted/data/remote/ApiUtils.kt
+++ Currency Converted/app/src/main/java/com/example/currency_converted/data/remote/ApiUtils.kt
@@ -3,8 +3,8 @@ package com.example.currency_converted.data.remote
 class ApiUtils {
 
     companion object {
-        private val API_KEY = "8c4f99e2c691a3a2f2892b2d"
-        private val BASE_URL = "https://v6.exchangerate-api.com/v6/${API_KEY}/"
+        private const val API_KEY = "8c4f99e2c691a3a2f2892b2d"
+        private const val BASE_URL = "https://v6.exchangerate-api.com/v6/${API_KEY}/"
 
         fun getApiService(): APIService {
             return RetrofitClient.getClient(BASE_URL).create(APIService::class.java)
